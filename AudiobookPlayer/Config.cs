a95970a46b81d513eff8d7ab8513521d13416d98
b0wter <evil-klabautermann@gmx.de>
2014-05-27 16:41:07 +0200

    cleaned config class. dataerrorinfo and notifypropertychanged no longer required. user settings now work on a per user basis.

--- AudiobookPlayer/Config.cs
+++ AudiobookPlayer/Config.cs
@@ -11,7 +11,7 @@ namespace AudiobookPlayer
 {
 	//TODO: Klasse noch einmal hübscher machen. Das Auslesen der Werte ist so nicht sehr elegant und kann Fehler nach sich ziehen, siehe entsprechenden Kommentar.
 	//
-	public class Config : IDataErrorInfo, INotifyPropertyChanged
+	public class Config //: IDataErrorInfo, INotifyPropertyChanged
 	{
 		const string UPDATE_INTERVALL_IDENT = "update_intervall_seconds";
 		const string SMALL_SKIP_SECONDS_IDENT = "small_skip_seconds";
@@ -27,7 +27,6 @@ namespace AudiobookPlayer
 
 		public Config()
 		{
-			SetDefaultValues();
 			ReadConfigValues();
 		}
 
@@ -46,91 +45,22 @@ namespace AudiobookPlayer
 
 		public void SaveConfig()
 		{
-			ConfigurationManager.AppSettings[UPDATE_INTERVALL_IDENT] = audiobook_update_intervall.ToString();
-			ConfigurationManager.AppSettings[SMALL_SKIP_SECONDS_IDENT] = small_skip_seconds.ToString();
-			ConfigurationManager.AppSettings[LARGE_SKIP_SECONDS_IDENT] = large_skip_seconds.ToString();
-			ConfigurationManager.AppSettings[NO_OF_THREADS_IDENT] = no_of_background_threads.ToString();
-			ConfigurationManager.AppSettings[AUDIOBOOK_PATH] = audiobook_path;
-		}
-
-		private void SetDefaultValues()
-		{
-			audiobook_update_intervall = 1.0;
-			small_skip_seconds = 60.0;
-			large_skip_seconds = 600;
-			no_of_background_threads = 2;
-			audiobook_path = "..\\..\\..\\..\\Audiobooks";
+			Properties.Settings.Default.AudiobookUpdateIntervall = audiobook_update_intervall;
+			Properties.Settings.Default.SmallSkipSize = small_skip_seconds;
+			Properties.Settings.Default.LargeSkipSize = large_skip_seconds;
+			Properties.Settings.Default.BackgroundThreads = no_of_background_threads;
+			Properties.Settings.Default.AudiobookPath = audiobook_path;
+			Properties.Settings.Default.Save();
 		}
 
 		private void ReadConfigValues()
 		{
-			//TODO: Wenn ein Key nicht gefunden wird, wird abgebrochen! Dies sollte umgangen werden.
-			bool init_failed = false;
-
-			init_failed = init_failed || !double.TryParse(ConfigurationManager.AppSettings[UPDATE_INTERVALL_IDENT],out audiobook_update_intervall);
-			init_failed = init_failed || !double.TryParse(ConfigurationManager.AppSettings[SMALL_SKIP_SECONDS_IDENT], out small_skip_seconds);
-			init_failed = init_failed || !double.TryParse(ConfigurationManager.AppSettings[LARGE_SKIP_SECONDS_IDENT], out large_skip_seconds);
-			init_failed = init_failed || !int.TryParse(ConfigurationManager.AppSettings[NO_OF_THREADS_IDENT], out no_of_background_threads);
-			audiobook_path = ConfigurationManager.AppSettings[AUDIOBOOK_PATH];
-
-			if (init_failed)
-				System.Windows.Forms.MessageBox.Show("At least one config value if of the wrong format. Using default value.", System.Windows.Forms.Application.ProductName, System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Warning);
+			audiobook_update_intervall = Properties.Settings.Default.AudiobookUpdateIntervall;
+			small_skip_seconds = Properties.Settings.Default.SmallSkipSize;
+			large_skip_seconds = Properties.Settings.Default.LargeSkipSize;
+			no_of_background_threads = Properties.Settings.Default.BackgroundThreads;
+			audiobook_path = Properties.Settings.Default.AudiobookPath;
 		}
-
-		private T GetConfigSetting<T>(string identifier)
-		{
-			string setting = ConfigurationManager.AppSettings[identifier];
-			if(typeof(T).IsSubclassOf(typeof(Enum)))
-			{
-				return (T)Enum.Parse(typeof(T), setting, true);
-			}
-
-			if (!String.IsNullOrEmpty(setting))
-				return (T)Convert.ChangeType(setting, typeof(T));
-
-			return default(T);
-		}
-
-		#region INotifyPropertyChanged Members
-		public event PropertyChangedEventHandler PropertyChanged;
-
-		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
-		{
-			if (this.PropertyChanged != null)
-				this.PropertyChanged(this, e);
-		}
-		#endregion
-
-		#region IDataErrorInfo Members
-		public string Error
-		{ get { return this[null]; } }
-
-		public string this[string property_name]
-		{
-			get
-			{
-				string result = string.Empty;
-				property_name = property_name ?? string.Empty;
-				if(property_name == string.Empty || property_name == "AudiobookPath")
-				{
-					if(string.IsNullOrEmpty(this.AudiobookPath))
-					{
-						result += "Audiobook path cannot be empty." + Environment.NewLine;
-					}
-				}
-
-				if(property_name == string.Empty || property_name == "AudiobookUpdateIntervall")
-				{
-					if(AudiobookUpdateIntervall < 1)
-					{
-						result += "Audiobook update intervall cannot be less than one." + Environment.NewLine;
-					}
-				}
-
-				return result.TrimEnd();
-			}
-		}
-		#endregion
 
 		#region Properties
 		public double AudiobookUpdateIntervall
@@ -139,21 +69,6 @@ namespace AudiobookPlayer
 			set 
 			{ 
 				audiobook_update_intervall = value;
-				this.OnPropertyChanged(new PropertyChangedEventArgs("AudiobookUpdateIntervall"));
-			}
-		}
-
-		public string AudiobookUpdateIntervallAsString
-		{
-			get { return audiobook_update_intervall.ToString(); }
-			set 
-			{ 
-				double new_value = -1.0;
-				if(!double.TryParse(value, out new_value))
-				{
-					throw new ApplicationException("Cannot convert string to double.");
-				}
-				AudiobookUpdateIntervall = new_value;
 			}
 		}
 
--- AudiobookPlayer/Properties/Settings.Designer.cs
+++ AudiobookPlayer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace AudiobookPlayer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,6 +20,66 @@ namespace AudiobookPlayer.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        public double AudiobookUpdateIntervall {
+            get {
+                return ((double)(this["AudiobookUpdateIntervall"]));
+            }
+            set {
+                this["AudiobookUpdateIntervall"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("30")]
+        public double SmallSkipSize {
+            get {
+                return ((double)(this["SmallSkipSize"]));
+            }
+            set {
+                this["SmallSkipSize"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("300")]
+        public double LargeSkipSize {
+            get {
+                return ((double)(this["LargeSkipSize"]));
+            }
+            set {
+                this["LargeSkipSize"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Audiobooks")]
+        public string AudiobookPath {
+            get {
+                return ((string)(this["AudiobookPath"]));
+            }
+            set {
+                this["AudiobookPath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        public int BackgroundThreads {
+            get {
+                return ((int)(this["BackgroundThreads"]));
+            }
+            set {
+                this["BackgroundThreads"] = value;
             }
         }
     }
